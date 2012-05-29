a6676053f1530e97bc680f6bd5a780a5f8d67540
iaddict <ts@image-addicts.de>
2012-05-29 12:00:30 +0200

    * fix load path

--- lib/clearcase_helper.rb
+++ lib/clearcase_helper.rb
@@ -1,6 +1,8 @@
-require "./version"
-require "./clearcase_helper/view"
-require "./clearcase_helper/cc_file"
+$:.push File.expand_path("..", __FILE__)
+
+require "version"
+require "clearcase_helper/view"
+require "clearcase_helper/cc_file"
 
 module ClearcaseHelper
 end
